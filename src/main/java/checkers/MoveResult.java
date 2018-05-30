22feef0f4a03f4b467922ed7ba0072355f39823d
Stefan Nyffenegger <>
2018-05-31 00:57:24 +0200

    Small stuff

--- src/main/java/checkers/MoveResult.java
+++ src/main/java/checkers/MoveResult.java
@@ -17,6 +17,7 @@ public class MoveResult {
      * Konstruktor mit einem Argument
      * MoveType NONE oder NORMAL wird festgesetzt, jedoch kein Spielstein übergeben
      *
+     * @param type Bewegungstyp
      */
     public MoveResult(MoveType type) {
         this(type, null);
@@ -26,6 +27,8 @@ public class MoveResult {
      * Konstruktor mit zwei Argumente für den MoveType KILL
      * Dabei wird der zu fressende Spielstein übergeben
      *
+     * @param type Bewegungstyp
+     * @param piece Spielstein
      */
     public MoveResult(MoveType type, Piece piece) {
         this.type = type;
